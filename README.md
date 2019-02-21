33755652bb712c7c5ecbcc773cbd6fb505d4bd63
Simone Ciccolella <s.ciccolella@campus.unimib.it>
2019-02-21 22:25:40 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # celluloid
 Celluloid: clustering single cell sequencing data around centroids
+
+How to install
+-----------------
+Install the dependecies of the tool, with `pip3 install kmodes` and then just clone this repository:
+
+```bash
+git clone https://github.com/AlgoLab/celluloid.git
+cd celluloid
+```
+
+Input formats
+-----------------
+**Single Cell file**
+
+The input file (specified by the `-i` parameter) is expected to be a ternary matrix file where the rows represent the cells and the columns the mutations. Each cell must be separated by a space. Each cell of the matrix can be:
+
+| Value of cell | Meaning |
+| ------------- | ------------- |
+| I[i,j] = 0    | Mutation *j* is not observed in cell *i*  |
+| I[i,j] = 1    | Mutation *j* is observed in cell *i*  |
+| I[i,j] = 2    | There is no information for mutation *j* in cell *i*, i.e. low coverage  |
+
+**Mutations file**
+
+This optional file specifies the name of the mutations (parameter `-l`). Each mutation's name must be on a different line (separated by `\n`), and the names are assigned to columns from left to right in the input file. If this file is not provided, mutations are progressively named from `1` to the total number of mutations.
+
+Output formats
+-----------------
+Celluloid will output one _Single Cell file_ and one _Mutations file_ in the same format as the input but with a number of mutations reduced by the clustering. Labels of mutation are concatenated in the out label file.
+
+
+How to run
+-----------------
+```
+usage: celluloid.py [-h] -i INPUT [-m METHOD] [-d DISSIM] [-n N] -k K
+                    [-l LABELS] -o OUTDIR [-v]
+
+Celluloid: clustering single cell sequencing data around centroids
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        input file
+  -m METHOD, --method METHOD
+                        initialization method
+  -d DISSIM, --dissim DISSIM
+                        dissimilarity function
+  -n N, --n_inits N     number of iterations
+  -k K, --kmodes K      number of modes
+  -l LABELS, --labels LABELS
+                        label file
+  -o OUTDIR, --outdir OUTDIR
+                        output directory.
+  -v, --verbose         verbose
+
+```
--- celluloid.py
+++ celluloid.py
@@ -1,44 +1,12 @@
 import sys
 import argparse
 import numpy as np
-from kmodes.kmodes.kmodes import KModes
-from kmodes.kmodes.util.dissim import matching_dissim, ng_dissim
+from kmodes.kmodes import KModes
+from kmodes.util.dissim import matching_dissim, ng_dissim
 from collections import defaultdict
 
-# a 'weak' form of matching for (e.g., SCS) matrices --- in {0,1,2}
-# --- which is the same as the matching_dissim, but does not count
-# missing values --- 2's in this case
 def conflict_dissim(a, b, **_) :
-
     v = np.vectorize(lambda ai, bi : ai != 2 and bi != 2 and ai != bi)
-
-    return np.sum(v(a,b), axis = 1)
-
-# likelihood that column a orignated from centroid b, according to
-# false negative/positive rates, same as I from E in SASC, expressed
-# as a dissimilarity
-alpha = 0.1
-beta = 0.001 # default values
-def likelihood_dissim(a, b, **_) :
-
-    # c.f. P(I|E) from SASC
-    def p(i,e) :
-
-        if i == 2 : return 1. # missing value, no contribution
-
-        if e == 0 :
-            return 1. - beta if i == 0 else beta
-        if e == 1 :
-            return alpha if i == 0 else 1. - alpha
-
-        # case not in SASC, but we need to handle it here, since e can
-        # be 2, we want a very lowwww.. probability when i is not 2
-        return min(alpha, beta) / 1000. # e == 2 and i != 2
-
-    # note: since we want a _dissimilarity_, we want this to be low
-    # when the (log) likelihood is high, and vice versa
-    v = np.vectorize(lambda i,e : -1. * np.log(p(i,e)))
-
     return np.sum(v(a,b), axis = 1)
 
 # choose dissimilarity function
@@ -49,8 +17,8 @@ def choose_dissim(dstr) :
         return ng_dissim
     if dstr == 'conflict' :
         return conflict_dissim
-    if dstr == 'likelihood' :
-        return likelihood_dissim
+    # if dstr == 'likelihood' :
+    #     return likelihood_dissim
 
     return None
 
@@ -72,8 +40,7 @@ def obtain_rates(filename) :
 
 parser = argparse.ArgumentParser(description = '''
 
-   run k-modes (https://github.com/nicodv/kmodes) on the columns of a
-   datset
+   Celluloid: clustering single cell sequencing data around centroids
 
 ''')
 
@@ -81,16 +48,17 @@ parser.add_argument(
     '-i', '--input',
     metavar = 'INPUT', dest = 'input',
     type = str, default = sys.stdin,
-    help = 'input file')
+    help = 'input file',
+    required=True)
 parser.add_argument(
     '-m', '--method',
     metavar = 'METHOD', dest = 'method',
-    type = str, default = 'cao',
+    type = str, default = 'huang',
     help = 'initialization method')
 parser.add_argument(
     '-d', '--dissim',
     metavar = 'DISSIM', dest = 'dissim',
-    type = str, default = 'matching',
+    type = str, default = 'conflict',
     help = 'dissimilarity function')
 parser.add_argument(
     '-n', '--n_inits',
@@ -100,18 +68,18 @@ parser.add_argument(
 parser.add_argument(
     '-k', '--kmodes',
     metavar = 'K', dest = 'k',
-    type = int, default = 10,
+    type = int, required = True,
     help = 'number of modes')
+
 parser.add_argument(
-    '-r', '--rates',
-    metavar = 'RATES', dest = 'rates',
-    type = str,
-    help = 'file containing false negative/positive rates')
-parser.add_argument(
-    '-c', '--centroids',
-    metavar = 'CENTROIDS', dest = 'centroids',
-    type = str,
-    help = 'file to which the centroids are output')
+    '-l', '--labels',
+    metavar = 'LABELS', dest = 'labels',
+    type = str, default = None,
+    help = 'label file')
+parser.add_argument('-o', 
+    '--outdir', action='store', 
+    type=str, required=True,
+    help='output directory.')
 parser.add_argument(
     '-v', '--verbose',
     dest = 'verbose',
@@ -124,14 +92,29 @@ args = parser.parse_args()
 # Main
 #----------------------------------------------------------------------
 
-# false negative/positve rates
-if args.dissim == 'likelihood' :
-    alpha, beta = obtain_rates(args.rates)
-    print('FN rate (alpha) =', alpha)
-    print('FP rate (beta) =', beta)
+import os, errno
+try:
+    os.makedirs(args.outdir)
+except OSError as exc:
+    if exc.errno == errno.EEXIST and os.path.isdir(args.outdir):
+        pass
+    else:
+        raise
 
 # load (columns of) dataset
 a = np.loadtxt(args.input, dtype = 'int').transpose()
+
+
+# load (or create) labels
+snv_labels = list()
+if args.labels:
+    with open(args.labels, 'r') as fin:
+        for line in fin:
+            snv_labels.append(line.strip()) 
+    if len(snv_labels) != a.shape[0]:
+        sys.exit('SCS file and LABELS do not have the same number of mutations.')
+else:
+    snv_labels = [str(x) for x in range(1, a.shape[0] + 1)]
 
 # to trigger the setting of n_init to 1 (small bug, really)
 method = args.method
@@ -147,21 +130,30 @@ km = KModes(
 
 clusters = km.fit_predict(a)
 
-# gather clusters
+# Print the cluster centroids
+
+cs = km.cluster_centroids_
+out_matrix = list()
+for c in cs :
+    out_matrix.append(c)
+out_matrix = np.array(out_matrix).transpose()
+
+# Output clustered matrix
+filename, ext = os.path.splitext(os.path.basename(args.input))
+scs_outfile = os.path.join(args.outdir, filename + '_clustered' + ext)
+np.savetxt(scs_outfile, out_matrix, fmt='%d', delimiter=' ')
+
+# output mutation names
+filename, ext = os.path.splitext(os.path.basename(args.labels))
+labels_outfile = os.path.join(args.outdir, filename + '_clustered' + ext)
+
 labels = km.labels_
 d = defaultdict(list)
-i = 1
+i = 0
 for label in labels :
-    d[int(label)].append(str(i))
+    d[int(label)].append(snv_labels[i])
     i += 1
 
-# output clusters in the agreed format
-for key in d :
-    print(key, '"'+','.join(d[key])+'"', sep = '\t', file = sys.stderr)
-
-# Print the cluster centroids
-c_file = open(args.centroids, 'w')
-cs = km.cluster_centroids_
-for c in cs :
-    print(*c, file = c_file)
-c_file.close()
+with open(labels_outfile, 'w+') as fout:
+    for key in sorted(d) :
+        fout.write('%s\n' % ','.join(d[key]))
